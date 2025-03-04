76158c1d6082b20dd5ceff08497b26935ccd27a6
ayushsharma-1 <ayushsharma18001@gmail.com>
2025-03-04 12:16:49 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';  // Separate React
-            if (id.includes('lodash')) return 'lodash-vendor'; // Separate Lodash
-            return 'vendor'; // Default vendor chunk
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Optional: Increases warning limit (default is 500KB)
-  }
-});
+  plugins: [react()],
+})
