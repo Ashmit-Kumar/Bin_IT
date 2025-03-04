9a0e6af5a65501c7c136516516a5aba097264163
ayushsharma-1 <ayushsharma18001@gmail.com>
2025-03-04 11:39:41 +0530

    Cache ckear

--- app/App.jsx
+++ app/App.jsx
@@ -3,13 +3,11 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import About from "../src/jsx/AboutPage/about.jsx";
 import Faqs from '../src/jsx/CommunityPages/faqs';
 import News from '../src/jsx/NewsPage/news';
-import Events from '../src/jsx/CommunityPages/events.jsx';
 import Home from '../src/jsx/HomePage/home';
 import NotFound from '../src/jsx/Commen-Components/NotFound.jsx';
-import EventParticipate from '../src/jsx/CommunityPages/event-participation.jsx';
-import EventCreate from '../src/jsx/CommunityPages/event-create.jsx';
 import ReportPage from '../src/jsx/ReportPage/report.jsx';
 import Ngo from '../src/jsx/RegistrationPages/ngo.jsx';
+
 
 function App() {
     return (
@@ -17,14 +15,11 @@ function App() {
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/events" element={<Events />} />
             <Route path="/news" element={<News />} />
             <Route path="/faqs" element={<Faqs />} />
-            <Route path="/participate-event" element={<EventParticipate />} />
-            <Route path="/create-event" element={<EventCreate />} />
             <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
             <Route path="/ngo-registration" element={<Ngo />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
     );
 }
