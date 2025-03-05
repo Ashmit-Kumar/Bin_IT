1fc92d4382fb6f9515a57ba8e19857792fb7e1a7
Ashmit <ashmitkumar1020@gmail.com>
2025-03-05 13:52:50 +0530

    Update hashrouter

--- app/App.jsx
+++ app/App.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import {  HashRouter as Router,Route, Routes, Navigate } from 'react-router-dom';
 import About from "../src/jsx/AboutPage/about.jsx";
 import Faqs from '../src/jsx/CommunityPages/faqs';
 import News from '../src/jsx/NewsPage/news';
 import Home from '../src/jsx/HomePage/home';
-import NotFound from '../src/jsx/Commen-Components/NotFound.jsx';
+// import NotFound from '../src/jsx/Commen-Components/NotFound.jsx';
 import ReportPage from '../src/jsx/ReportPage/report.jsx';
 import Ngo from '../src/jsx/RegistrationPages/ngo.jsx';
 
 
+// function App() {
+//     return (
+//         <Routes>
+//             <Route path="#/" element={<Home />} />
+//             <Route path="#/about" element={<About />} />
+//             <Route path="#/news" element={<News />} />
+//             <Route path="#/faqs" element={<Faqs />} />
+//             <Route path="#/submit-report" element={<ReportPage totalPages={4} />} />
+//             <Route path="#/ngo-registration" element={<Ngo />} />
+//             <Route path="*" element={<Home />} />
+//         </Routes>
+//     );
+// }
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/faqs" element={<Faqs />} />
-            <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
-            <Route path="/ngo-registration" element={<Ngo />} />
-            <Route path="*" element={<Home />} />
-        </Routes>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/faqs" element={<Faqs />} />
+                <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
+                <Route path="/ngo-registration" element={<Ngo />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
+        </Router>
     );
 }
-
 export default App;
